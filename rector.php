092ea113d50b17eb679713ea880fb3db911ecf91
Thomas Eimers <eimers@mehrkanal.com>
2024-09-11 08:18:51 +0200

    Add rector

--- rector.php
+++ rector.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
-use Rector\Php73\Rector\FuncCall\SetCookieRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return static function (RectorConfig $rectorConfig): void {
     // Ensure file system caching is used instead of in-memory.
